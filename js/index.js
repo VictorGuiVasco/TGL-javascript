9ac6b1191efcfa60d026633bd47d03587e8aff5d
VictorGuiVasco <victorguilhermecavalcante@gmail.com>
2021-06-16 18:11:08 -0300

    feed: feature to save on cart added

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@
 
   var numbersSelectedLength = 0
   var numbersSelected = []
+  var actualRange = 0
+  var actualPrice = 0
 
   var app = (() => {
     var rules = {}
@@ -24,21 +26,29 @@
       },
 
       initButtonsEvents: function initButtonsEvents() {
-        DOM('.complete-game').on('click', this.c, false)
+        DOM('.complete-game').on('click', this.handleCompleteGame, false)
         DOM('.clear-game').on('click', this.handleClickClearGame, false)
-        DOM('.cart-button').on('click', this.c, false)
+        DOM('.cart-button').on('click', this.handleClickCart, false)
       },
 
       initNumbersEvents: function initNumbersEvents() {
         DOM('.number-card').on('click', this.handleClickBetNumber, false)
       },
 
+      handleCompleteGame: function handleCompleteGame() {
+        var numOfEmptySpaces = numbersSelectedLength - numbersSelected.length
+        for (var i = 0; i < numOfEmptySpaces; i++) {
+          numbersSelected.push(app().getRandomIntInclusive(actualRange))
+        }
+        numbersSelected.sort(app().compare)
+      },
+
       handleClickClearGame: function handleClickBetNumber() {
         numbersSelected = []
       },
 
-      c: function c() {
-        console.log(this.textContent)
+      handleClickCart: function handleClickCart() {
+        app().addToCart()
       },
 
       openConnection: function openConnection() {
@@ -75,12 +85,12 @@
           app().showBetInfo(megasena)
         }
         if ('Lotomania' === this.textContent) {
-          app().changeBackgroundColor(this, lotomania)
+          app().changeBackgroundColor(this)
           app().showBetInfo(lotomania)
         }
       },
 
-      changeBackgroundColor: function changeBackgroundColor($button, data) {
+      changeBackgroundColor: function changeBackgroundColor($button) {
         var $lotofacilButton = DOM('[data-js="lotofacil-button"]').get()[0]
         var $megaSenaButton = DOM('[data-js="mega-sena-button"]').get()[0]
         var $lotomaniaButton = DOM('[data-js="lotomania-button"]').get()[0]
@@ -109,6 +119,7 @@
       showBetInfo: function showBetInfo(game) {
         var { ['max-number']: maxNumber } = game
         numbersSelectedLength = maxNumber
+        actualRange = game.range
 
         var $titleContainer = doc.getElementsByClassName('title')[0]
         var $titleGameOld = doc.getElementsByClassName('game-name')[0]
@@ -208,10 +219,127 @@
           console.log('array cheio')
         }
         else {
-          numbersSelected.push(this.textContent)
+          numbersSelected.push(Number(this.textContent))
         }
         console.log(numbersSelected)
-      }
+      },
+
+      getRandomIntInclusive: function getRandomIntInclusive(max) {
+        var num = Math.ceil(Math.random() * max + 1)
+        while (numbersSelected.indexOf(num) >= 0) {
+          num = Math.ceil(Math.random() * max + 1)
+        }
+        return num
+      },
+
+      compare: function compare(num1, num2) {
+        if (num1 > num2) return 1
+        if (num1 < num2) return -1
+        return 0
+      },
+
+      addToCart: function addToCart() {
+        if (!app().isComplete()) {
+          return
+        }
+        var $card = doc.getElementsByClassName('cart-betting-sheet')[0]
+        var $price = doc.getElementsByClassName('total-price-p')[0]
+        var $bet = doc.createElement('div')
+
+        $bet.className = 'betting-sheet-game'
+        $bet.appendChild(app().createTrahsButton())
+        $bet.appendChild(app().createBetContaianer())
+
+        $price.textContent = `TOTAL: R$ ${actualPrice}`
+        $card.appendChild($bet)
+      },
+
+      createTrahsButton: function createTrahsButton() {
+        var $delete = doc.createElement('button')
+        var $trashImg = doc.createElement('img')
+
+        $delete.className = 'delete-bet'
+        $trashImg.src = 'images/icons/trash.svg'
+        $trashImg.alt = 'deletar'
+
+        $delete.appendChild($trashImg)
+        $delete.addEventListener('click', app().removeBet)
+
+        return $delete
+      },
+
+      createBetContaianer: function createBetContaianer() {
+        var $bet = doc.createElement('div')
+        var $bar = doc.createElement('div')
+
+        var $betCard = doc.createElement('div')
+        var $numbers = doc.createElement('p')
+
+        var $betPrice = doc.createElement('div')
+        var $gameName = doc.createElement('p')
+        var $price = doc.createElement('p')
+
+        $bet.className = 'bet'
+        $betCard.className = 'bet-card'
+
+        $numbers.className = 'numbers'
+        $numbers.textContent = numbersSelected.join(', ')
+        $betPrice.className = 'bet-price'
+
+        $price.className = 'price'
+
+        if (numbersSelectedLength === 15) {
+          $bar.className = 'pourple-bar'
+          $gameName.className = 'lotofacil'
+          $gameName.textContent = 'Lotofácil'
+          $price.textContent += 'R$ 2,50'
+          actualPrice += 2.50
+        }
+        else if (numbersSelectedLength === 6) {
+          $bar.className = 'green-bar'
+          $gameName.className = 'mega-sena'
+          $gameName.textContent = 'Mega-Sena'
+          $price.textContent += 'R$ 4,50'
+          actualPrice += 4.50
+        }
+        else if (numbersSelectedLength === 5) {
+          $bar.className = 'orange-bar'
+          $gameName.className = 'lotomania'
+          $gameName.textContent = 'Lotofácil'
+          $price.textContent += 'R$ 2,00'
+          actualPrice += 2.00
+        }
+
+        $betPrice.appendChild($gameName)
+        $betPrice.appendChild($price)
+        $betCard.appendChild($numbers)
+        $betCard.appendChild($betPrice)
+        $bet.appendChild($bar)
+        $bet.appendChild($betCard)
+
+        return $bet
+      },
+
+      isComplete: function isComplete() {
+        return numbersSelected.length === numbersSelectedLength
+      },
+
+      removeBet: function removeBet() {
+        var $price = doc.getElementsByClassName('total-price-p')[0]
+        if (this.parentNode) {
+          if ('pourple-bar' === this.parentNode.lastElementChild.firstElementChild.className) {
+            actualPrice -= 2.50
+          }
+          if ('green-bar' === this.parentNode.lastElementChild.firstElementChild.className) {
+            actualPrice -= 4.50
+          }
+          if ('orange-bar' === this.parentNode.lastElementChild.firstElementChild.className) {
+            actualPrice -= 2.00
+          }
+          $price.textContent = `TOTAL: R$ ${actualPrice}`
+          this.parentNode.parentNode.removeChild(this.parentNode)
+        }
+      },
     }
   })
 
